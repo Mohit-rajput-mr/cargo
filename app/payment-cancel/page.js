6e9a8e4b813a74cf03b28e0fdd7bf87cec908fb9
Mohit-rajput-mr <rajputmohit192003@gmail.com>
2025-04-14 09:53:21 +0200

    admin stripe

--- app/payment-cancel/page.js
+++ app/payment-cancel/page.js
@@ -14,13 +14,13 @@ export default function PaymentCancel() {
             backgroundColor: "#fff",
             padding: "3rem 4rem",
             borderRadius: "16px",
-            boxShadow: "0 0 30px rgba(0, 0, 0, 0.2)",
+            boxShadow: "0 0 30px rgba(0, 0, 0, 0.14)",
             textAlign: "center",
           }}
         >
           <h1 style={{ fontSize: "2rem", color: "#ff370a" }}>❌ Payment Cancelled!</h1>
           <p style={{ fontSize: "1.1rem", marginTop: "0.8rem", color: "#333" }}>
-            Please check your bank account ensure if you have sufficient amount! 😬
+            Please check your bank account ensure if you have sufficient amount! 
           </p>
         </div>
       </div>
